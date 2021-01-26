8a1c651fde0d0639ecefc69bf755e6b0c9017e4d
agarwalhimanshu <aggrwalhimanshu@users.noreply.github.com>
2021-01-26 14:10:16 -0800

    fixing for heroku

--- airline_manager/settings.py
+++ airline_manager/settings.py
@@ -117,7 +117,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
